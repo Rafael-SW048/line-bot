cd83a87db0e8a553511fd58c7fd1589dbc06b540
Rafael Sinjunatha Wulangsih <18120048@std.stei.itb.ac.id>
2022-05-15 20:54:55 +0700

    Added LINE Massager API on bot.py and add virtualenv+lib  for python

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
-# from __future__ import print_function
-# import pickle
-# import os.path
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
-from sheetfu import SpreadsheetApp
+import os
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SERVICE_ACCOUNT_FILE = 'keys.json'
@@ -20,29 +25,58 @@ CREDS = service_account.Credentials.from_service_account_file(
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1OSZXvGldMHw4fgkMMinsAv0L19W_U_8ZmFVVz-DFohw'
-# SAMPLE_RANGE_NAME = 'B1:F'
+SAMPLE_RANGE_NAME = 'B1:F'
 
-service = SpreadsheetApp(SERVICE_ACCOUNT_FILE)
-file = service.open_by_id(spreadsheet_id=SPREADSHEET_ID)
-sheat = file.get_sheet_by_name('test-sheet')
-result = sheat.get_data_range()
-values = result.get_values()
-print(values)
+try:
+    service = build('sheets', 'v4', credentials=CREDS)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+
+    else:
+        print(values)   
+
+except HttpError as err:
+    print(err)
+
+# LINE Massager API
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('YXLm/vqJIXGcmuNqyaEu5yYf/l8ElZ1fFm/8rmDbNhwea4bT5Cm4x1qKdm3FyIVWdxXmCX6osVoMtxhR1YJBk8sKzIpliaOgT4MAxqLvPdmBCCoRQNkc/TSu3du9x8yfLT8SDCO5OGtjaIKyDwpoIQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('651cab7325f1467ef97ad60952d5b0e6')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # Get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # Get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # Handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
-# try:
-#     service = build('sheets', 'v4', credentials=CREDS)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    """ Here's all the messages will be handled and processed by the program """
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
-#     # Call the Sheets API
-#     sheet = service.spreadsheets()
-#     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-#                                 range=SAMPLE_RANGE_NAME).execute()
-#     values = result.get('values', [])
 
-#     if not values:
-#         print('No data found.')
-
-#     else:
-#         print(values)
-# except HttpError as err:
-#     print(err)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-creds
+
+# from linebot.exceptions import (
